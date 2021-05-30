5b6e0a34b0fcd21cffe0eb8c57d2fc68f9614150
CodeMasterBoom <boomhacker@cryptopup.space>
2021-05-30 04:27:33 +0000

    encode only content

--- BitGanjTelegraph.js
+++ BitGanjTelegraph.js
@@ -8,12 +8,12 @@ function BitGanjTelegraph(v_access_token,v_author_name, v_author_url) {
 BitGanjTelegraph.prototype.createPage = function (pEntry) {
     var vCe = pEntry !== undefined ? pEntry : entry();
     var res = false;
-    var vTitle = vCe.field("ContentInfo");
+    var vTitle = vCe.field("title");
     var vContent = '[{"tag":"p","attrs":{},"children":[{"tag":"br","attrs":{},"children":[]}]},{"tag":"figure","attrs":{},"children":[{"tag":"img","attrs":{"src":"https://telegra.ph/file/2ff9ee4b8b9c9218ca074.jpg"},"children":[]},{"tag":"figcaption","attrs":{},"children":[]}]}]';
-    var params = 'title='+vTitle+'&author_name='+this.author_name+'&author_url='+this.author_url+'&content='+vContent;
+    var params = 'title='+vTitle+'&author_name='+this.author_name+'&author_url='+this.author_url+'&content=';
     var vURI = "https://api.telegra.ph/createPage?access_token="+access_token;
     log (params);
-    var vBody = encodeURIComponent(params);
+    var vBody = params+encodeURIComponent(vContent);
     log (vBody);
     var vResult = http().post(vURI,vBody);
     log("Result code:" + vResult.code + " with body:" + vResult.body);
