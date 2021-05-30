087769399244981e459d8bae41506267798adbc3
CodeMasterBoom <boomhacker@cryptopup.space>
2021-05-30 05:15:44 +0000

    fix returning var

--- BitGanjTelegraph.js
+++ BitGanjTelegraph.js
@@ -9,18 +9,20 @@ BitGanjTelegraph.prototype.createPage = function (pEntry) {
     var vCe = pEntry !== undefined ? pEntry : entry();
     var res = false;
     var vTitle = vCe.field("title");
-    var vContent = '[{"tag":"p","attrs":{},"children":[{"tag":"br","attrs":{},"children":[]}]},{"tag":"figure","attrs":{},"children":[{"tag":"img","attrs":{"src":"https://telegra.ph/file/2ff9ee4b8b9c9218ca074.jpg"},"children":[]},{"tag":"figcaption","attrs":{},"children":[]}]}]';
+    var vContent = '[{"tag":"p","children":["Hello, world!"]}]';
     var params = 'title='+encodeURIComponent(vTitle)+'&author_name='+encodeURIComponent(this.author_name)+'&author_url='+encodeURIComponent(this.author_url)+'&content=';
     var vURI = "https://api.telegra.ph/createPage?access_token="+access_token;
-    log (params);
     var vBody = params+encodeURIComponent(vContent);
     log (vBody);
     var vResult = http().post(vURI,vBody);
     log("Result code:" + vResult.code + " with body:" + vResult.body);
-        if (vResult.code === 200) {
-          var res = JSON.parse(vResult.body);             
-        } else {
-        log ("ServerError:" + vResult.code);
+    if (vResult.code === 200) {
+        var json = JSON.parse(vResult.body);             
+       if (json.ok === true) 
+       {    
+           res = json.result;
+       } else { 
+           message("Error"+json.result); }
     };
     return res;
   };
