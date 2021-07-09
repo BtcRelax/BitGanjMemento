7ca8f598727a45029f4ffbf62ee5e53bd869bdc2
CodeMasterBoom <boomhacker@cryptopup.space>
2021-07-09 16:18:44 +0000

    update vendor after deal changed

--- BitGanjDeals.js
+++ BitGanjDeals.js
@@ -1,16 +1,24 @@
 /*eslint-disable no-undef, no-undef, no-undef*/
 function BitGanjDeals() {
+    this.current_date= moment().toDate();
 }
 
 BitGanjDeals.prototype.setEndDate = function(pEntry) {
     var ce= typeof pEntry !== "undefined" ? pEntry : entry();
     var vn = ce.field('State');
     var ct = ce.field('FinishDate');
-    var cdt=moment().toDate();
-
     if (vn=='Catched' || vn=='Canceled'){
-        if (ct==null) { entry().set("FinishDate",cdt); } 
+        if (ct==null) { entry().set("FinishDate",this.current_date); } 
     } else {
             entry().set("FinishDate",null);
+    };    
+}
+
+BitGanjDeals.prototype.updateVendor = function(pEntry) {
+    var ce= typeof pEntry !== "undefined" ? pEntry : entry();
+    if (ce.field("Vendor").length > 0)
+    {
+      var vVend=ce.field("Vendor")[0];
+      vVend.set("LastTransaction", this.current_date );
     }
 }
