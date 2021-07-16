0ea7b41c688645bc212ed14d48e2a643f435d517
CodeMasterBoom <boomhacker@cryptopup.space>
2021-07-16 02:10:22 +0000

    converting to class

--- BitGanjDeals.js
+++ BitGanjDeals.js
@@ -1,25 +1,26 @@
 /*eslint-disable no-undef, no-undef, no-undef*/
-function BitGanjDeals() {
-    this.current_date= moment().toDate();
-}
-
-BitGanjDeals.prototype.setEndDate = function(pEntry) {
-    var ce= typeof pEntry !== "undefined" ? pEntry : entry();
-    var vn = ce.field('State');
-    var ct = ce.field('FinishDate');
-    if (vn=='Catched' || vn=='Canceled'){
-        if (ct==null) { entry().set("FinishDate",this.current_date); } 
-    } else {
-            entry().set("FinishDate",null);
-    };    
-}
-
-BitGanjDeals.prototype.updateVendor = function(pEntry) {
-    var ce= typeof pEntry !== "undefined" ? pEntry : entry();
-    if (ce.field("Vendor").length > 0)
-    {
-      var vVend=ce.field("Vendor")[0];
-      vVend.set("LastTransaction", this.current_date );
-      log("Last transaction date for vendor was set to:"+this.current_date);
+class BitGanjDeals {
+    constructor() {
+        this.current_date = moment().toDate();
+    }
+    setEndDate(pEntry) {
+        var ce = typeof pEntry !== "undefined" ? pEntry : entry();
+        var vn = ce.field('State');
+        var ct = ce.field('FinishDate');
+        if (vn == 'Catched' || vn == 'Canceled') {
+            if (ct == null) { entry().set("FinishDate", this.current_date); }
+        } else {
+            entry().set("FinishDate", null);
+        };
+    }
+    updateVendor(pEntry) {
+        var ce = typeof pEntry !== "undefined" ? pEntry : entry();
+        if (ce.field("Vendor").length > 0) {
+            var vVend = ce.field("Vendor")[0];
+            vVend.set("LastTransaction", this.current_date);
+            log("Last transaction date for vendor was set to:" + this.current_date);
+        }
     }
 }
+
+
